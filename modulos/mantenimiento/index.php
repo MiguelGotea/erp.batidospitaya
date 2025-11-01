ddd3c459b602e4207a1d8cfb5beadc844d993a7d
BatidosPitaya <miguelgotea.1@gmail.com>
2025-10-31 21:07:24 -0600

    2025-10-31 21:07:24

--- modulos/mantenimiento/index.php
+++ modulos/mantenimiento/index.php
@@ -349,7 +349,7 @@ if (!verificarAccesoCargo(14)) {
         
         <div class="module-header">
             <h1 class="module-title-page">Área de Mantenimiento</h1>
-            <h1 class="module-title-page"><?$esAdmin?></h1>
+            <?$esAdmin?>
         </div>
 
         <?php
