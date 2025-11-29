b92f1d7a78e2cd961ee4a859fa893505813d727b
BatidosPitaya <miguelgotea.1@gmail.com>
2025-11-29 02:47:55 -0600

    2025-11-29 02:47:55

--- modulos/mantenimiento/programacion_solicitudes.php
+++ modulos/mantenimiento/programacion_solicitudes.php
@@ -1,4 +1,5 @@
 <?php
+$version = "1.0.3"; // Incrementa cuando hagas cambios
 // programacion_solicitudes.php
 require_once __DIR__ . '/config/database.php';
 require_once __DIR__ . '/models/Ticket.php';
@@ -151,7 +152,7 @@ $tickets_pendientes = $ticketModel->getTicketsWithoutDates();
     <title>Programación de Solicitudes</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.css">
-    <link rel="stylesheet" href="css/programacion_solicitudes.css">
+    <link rel="stylesheet" href="css/programacion_solicitudes.css?v=<?php echo $version; ?>">
 </head>
 <body>
     <div class="container-fluid p-3">
@@ -273,7 +274,7 @@ $tickets_pendientes = $ticketModel->getTicketsWithoutDates();
 
     <script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="js/programacion_solicitudes.js"></script>
+    <script src="js/programacion_solicitudes.js?v=<?php echo $version; ?>"></script>
     
     <script>
     // Datos de tickets para JavaScript
