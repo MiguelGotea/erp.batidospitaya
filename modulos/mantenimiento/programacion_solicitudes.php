cb4b7f5359972f012a527a695eccd06c7b7f9144
BatidosPitaya <miguelgotea.1@gmail.com>
2025-11-29 00:04:10 -0600

    2025-11-29 00:04:10

--- modulos/mantenimiento/programacion_solicitudes.php
+++ modulos/mantenimiento/programacion_solicitudes.php
@@ -151,7 +151,7 @@ $tickets_pendientes = $ticketModel->getTicketsWithoutDates();
     <title>Programación de Solicitudes</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.css">
-    <link rel="stylesheet" href="css/programacion_solicitudes.css">
+    <link rel="stylesheet" href="css/programacion_solicitudes">
 </head>
 <body>
     <div class="container-fluid p-3">
