ae19856163f2896857fc0d7551f7eccb0614cf8e
BatidosPitaya <miguelgotea.1@gmail.com>
2025-11-29 00:19:03 -0600

    2025-11-29 00:19:03

--- modulos/mantenimiento/programacion_solicitudes.php
+++ modulos/mantenimiento/programacion_solicitudes.php
@@ -273,16 +273,28 @@ $tickets_pendientes = $ticketModel->getTicketsWithoutDates();
 
     <script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="js/programacion_solicitudes.js"></script>
     
     <script>
     // Datos de tickets para JavaScript
     const ticketsPorEquipo = <?php echo json_encode($tickets_por_equipo, JSON_UNESCAPED_UNICODE); ?>;
     const fechasSemana = <?php echo json_encode($fechas, JSON_UNESCAPED_UNICODE); ?>;
     
+    console.log('Tickets por equipo:', ticketsPorEquipo);
+    console.log('Fechas semana:', fechasSemana);
+    </script>
+    
+    <script src="js/programacion_solicitudes.js"></script>
+    
+    <script>
     // Renderizar tickets en el cronograma
     document.addEventListener('DOMContentLoaded', function() {
-        renderizarCronograma();
+        console.log('DOM Cargado - Iniciando renderizado');
+        
+        // Pequeño delay para asegurar que todo esté listo
+        setTimeout(function() {
+            renderizarCronograma();
+            console.log('Renderizado completado');
+        }, 100);
     });
     </script>
 </body>
