831a82544f761a21a2085523e31a2cba8e2657b2
BatidosPitaya <miguelgotea.1@gmail.com>
2025-12-16 23:45:27 -0600

    2025-12-16 23:45:27

--- modulos/mantenimiento/agenda_colaborador.php
+++ modulos/mantenimiento/agenda_colaborador.php
@@ -50,6 +50,7 @@ if ($colaborador_filtro) {
     <title>Agenda de Colaboradores</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     <link rel="icon" href="../../assets/img/icon12.png" type="image/png">
+    
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
         * {
