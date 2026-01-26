fb2f3d80c115a10cfd744527d782a960ce8738c6
BatidosPitaya <miguelgotea.1@gmail.com>
2026-01-26 12:13:17 -0600

    2026-01-26 12:13:17

--- modulos/mantenimiento/equipos_registro.php
+++ modulos/mantenimiento/equipos_registro.php
@@ -1,5 +1,5 @@
 <?php
-require_once '../../includes/auth.php';
+require_once '../../core/auth/auth.php';
 require_once '../../includes/funciones.php';
 require_once __DIR__ . '/config/database.php';
 
