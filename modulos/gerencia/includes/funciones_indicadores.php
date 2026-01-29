700f91d6c4885204f948a6d5904843de63513ad3
BatidosPitaya <miguelgotea.1@gmail.com>
2026-01-29 06:19:07 -0600

    Update gerencia: 2026-01-29 06:19:07

--- modulos/gerencia/includes/funciones_indicadores.php
+++ modulos/gerencia/includes/funciones_indicadores.php
@@ -37,7 +37,8 @@ function formatearValor($valor, $tipo, $decimales, $enUso = 0)
 {
     // Si EnUso = 1 y valor es null/vacío, usar 0
     if ($enUso == 1 && ($valor === null || $valor === '')) {
-        $valor = 0;
+        //$valor = 0;
+        return '-';
     }
 
     if ($valor === null || $valor === '') {
