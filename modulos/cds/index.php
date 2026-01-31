9ed4da948eb88cbbadc1b7a56d3728974bee4616
BatidosPitaya <miguelgotea.1@gmail.com>
2026-01-30 18:13:12 -0600

    2026-01-30 18:13:12

--- modulos/cds/index.php
+++ modulos/cds/index.php
@@ -49,7 +49,7 @@ if (!tienePermiso('index_cds', 'vista', $cargoOperario)) {
         <?php echo renderMenuLateral($cargoOperario); ?>
         <div class="main-container">
             <div class="contenedor-principal">
-                <?php echo renderHeader($usuario, $esAdmin); ?>
+                <?php echo renderHeader($usuario, false); ?>
 
                 <h2 class="section-title">
                     <i class="fas fa-chart-line"></i> Indicadores de Control
