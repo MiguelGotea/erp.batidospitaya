60968b023b24000ccdb38e73906d2479642f6071
BatidosPitaya <miguelgotea.1@gmail.com>
2026-02-01 05:17:17 -0600

    Update rh: 2026-02-01 05:17:17

--- modulos/rh/editar_colaborador.php
+++ modulos/rh/editar_colaborador.php
@@ -5783,12 +5783,12 @@ if (isset($_POST['accion_liquidacion']) && $_POST['accion_liquidacion'] == 'asig
                                                                     <td style="padding: 10px; text-align: center;">
                                                                         <button style="display:none;" type="button"
                                                                             class="btn-accion btn-editar"
-                                                                            onclick="editarAdendum(<?= $adendum['id'] ?>)">
+                                                                            onclick="editarAdendum(<?= $adendum['CodAsignacionNivelesCargos'] ?>)">
                                                                             <i class="fas fa-edit"></i>
                                                                         </button>
-                                                                        <?php if (empty($adendum['FechaFin'])): ?>
+                                                                        <?php if (empty($adendum['Fin'])): ?>
                                                                             <button type="button" class="btn-accion"
-                                                                                onclick="abrirModalFinalizarAdenda(<?= $adendum['id'] ?>)"
+                                                                                onclick="abrirModalFinalizarAdenda(<?= $adendum['CodAsignacionNivelesCargos'] ?>)"
                                                                                 style="color: #dc3545; display:none;" title="Finalizar Adenda">
                                                                                 <i class="fas fa-flag-checkered"></i>
                                                                             </button>
