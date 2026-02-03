113d403cffbe5a812bba4e8503f891fad0986e03
BatidosPitaya <miguelgotea.1@gmail.com>
2026-02-03 11:56:17 -0600

    2026-02-03 11:56:17

--- modulos/rh/editar_colaborador.php
+++ modulos/rh/editar_colaborador.php
@@ -3382,6 +3382,7 @@ if (isset($_POST['accion_liquidacion']) && $_POST['accion_liquidacion'] == 'asig
 <!DOCTYPE html>
 <html lang="es">
 
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
