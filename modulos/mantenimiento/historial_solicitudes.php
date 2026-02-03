f92408d05578deab3cacda607f010cdbc80d6ed1
BatidosPitaya <miguelgotea.1@gmail.com>
2026-02-03 13:26:04 -0600

    Update mantenimiento: 2026-02-03 13:26:04

--- modulos/mantenimiento/historial_solicitudes.php
+++ modulos/mantenimiento/historial_solicitudes.php
@@ -90,7 +90,7 @@ function getTextoUrgencia($nivel)
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Historial de Solicitudes</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
-    <link rel="icon" href="../../assets/img/icon12.png" type="image/png">
+    <link rel="icon" href="../../core/assets/img/icon12.png" type="image/png">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.css">
