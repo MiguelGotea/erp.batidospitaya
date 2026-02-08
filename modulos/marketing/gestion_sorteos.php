71ff793418ee54c3ea2fc5e948f0b5f5b3430684
BatidosPitaya <miguelgotea.1@gmail.com>
2026-02-07 18:50:33 -0600

    2026-02-07 18:50:33

--- modulos/marketing/gestion_sorteos.php
+++ modulos/marketing/gestion_sorteos.php
@@ -26,7 +26,7 @@ if (!tienePermiso('gestion_sorteos', 'vista', $cargoOperario)) {
     <link rel="icon" href="../../assets/img/icon12.png" type="image/png">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.css">
-    <link rel="stylesheet" href="/assets/css/global_tools.css?v=<?php echo mt_rand(1, 10000); ?>">
+    <link rel="stylesheet" href="/core/assets/css/global_tools.css?v=<?php echo mt_rand(1, 10000); ?>">
     <style>
         .validado-badge {
             padding: 4px 12px;
