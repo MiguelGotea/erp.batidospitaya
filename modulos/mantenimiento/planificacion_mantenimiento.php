b579eeea8f25ac8aecc10983f116e4b19025b121
BatidosPitaya <miguelgotea.1@gmail.com>
2026-02-09 13:48:40 -0600

    2026-02-09 13:48:40

--- modulos/mantenimiento/planificacion_mantenimiento.php
+++ modulos/mantenimiento/planificacion_mantenimiento.php
@@ -8,7 +8,6 @@ require_once __DIR__ . '/../../core/layout/menu_lateral.php';
 require_once __DIR__ . '/../../core/layout/header_universal.php';
 require_once __DIR__ . '/../../core/permissions/permissions.php';
 
-
 $usuario = obtenerUsuarioActual();
 $cargoOperario = $usuario['CodNivelesCargos'];
 
