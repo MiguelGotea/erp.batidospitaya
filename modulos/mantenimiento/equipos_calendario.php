b72da800d79d6de202a2269c7a8b4c8ccc5a2029
BatidosPitaya <miguelgotea.1@gmail.com>
2026-02-09 10:38:42 -0600

    2026-02-09 10:38:42

--- modulos/mantenimiento/equipos_calendario.php
+++ modulos/mantenimiento/equipos_calendario.php
@@ -5,7 +5,6 @@ require_once '../../core/layout/header_universal.php';
 require_once '../../core/layout/menu_lateral.php';
 require_once __DIR__ . '/config/database.php';
 
-
 $usuario = obtenerUsuarioActual();
 $cargoOperario = $usuario['CodNivelesCargos'];
 
