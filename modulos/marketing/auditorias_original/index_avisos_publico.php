239c1b359335b0b83486fa7c5797785b5f24f5dd
BatidosPitaya <miguelgotea.1@gmail.com>
2026-02-09 00:53:02 -0600

    2026-02-09 00:53:02

--- modulos/marketing/auditorias_original/index_avisos_publico.php
+++ modulos/marketing/auditorias_original/index_avisos_publico.php
@@ -141,7 +141,7 @@ try {
     <link rel="stylesheet" href="styles.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     <link rel="stylesheet" href="/assets/css/global_tools.css?v=<?php echo $version; ?>"> <!-- contiene main, sub container * y body -->
-    <link rel="icon" href="../../../assets/icon12.png" type="image/png">
+    <link rel="icon" href="../../../core/assets/icon12.png" type="image/png">
     <style>
         /* Estilos para los avisos */
         .aviso {
