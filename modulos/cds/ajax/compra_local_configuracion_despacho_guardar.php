4e8ed8eb0577b06cbc5d50c598482cafef48e86d
BatidosPitaya <miguelgotea.1@gmail.com>
2026-02-11 06:16:02 -0600

    2026-02-11 06:16:02

--- modulos/cds/ajax/compra_local_configuracion_despacho_guardar.php
+++ modulos/cds/ajax/compra_local_configuracion_despacho_guardar.php
@@ -5,6 +5,9 @@
 require_once '../../../core/auth/auth.php';
 require_once '../../../core/database/conexion.php';
 
+// Configurar zona horaria de Managua
+date_default_timezone_set('America/Managua');
+
 header('Content-Type: application/json');
 
 try {
