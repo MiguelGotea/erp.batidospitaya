f3b0664703bc21f5704bb46e6b27d25dd91ce6d2
BatidosPitaya <miguelgotea.1@gmail.com>
2026-02-11 06:12:25 -0600

    2026-02-11 06:12:25

--- modulos/sucursales/index.php
+++ modulos/sucursales/index.php
@@ -2,6 +2,7 @@
 require_once '../../core/auth/auth.php';
 require_once '../../core/layout/header_universal.php';
 require_once '../../core/layout/menu_lateral.php';
+require_once '../../core/permissions/permissions.php';
 
 $usuario = obtenerUsuarioActual();
 $cargoOperario = $usuario['CodNivelesCargos'];
