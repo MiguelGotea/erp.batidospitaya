2bdf841d451323b811e7fd564d62372623b6a981
BatidosPitaya <miguelgotea.1@gmail.com>
2026-02-19 12:43:55 -0600

    2026-02-19 12:43:55

--- modulos/sistemas/prueba_ics.php
+++ modulos/sistemas/prueba_ics.php
@@ -17,7 +17,7 @@ $descripcion = "Esta es una reunión de prueba enviada automáticamente desde el
 $ubicacion = "Oficina Central / Google Meet";
 $organizador_nombre = "Miguel Gotea";
 $organizador_email = "mgotea@batidospitaya.com";
-$asistente_email = "miguel_gotea@hotmail.com"; // Cambiado a hotmail para evitar rebotes de spam por envío a sí mismo
+$asistente_email = "mgotea@batidospitaya.com"; // Cambiado a hotmail para evitar rebotes de spam por envío a sí mismo
 
 
 $fecha_inicio = date('Ymd\THis', strtotime('+24 hour')); // Programado para mañana para que parezca más real
