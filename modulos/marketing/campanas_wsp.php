54ebe367980a54084ed260446b5a4f15443b1cb5
BatidosPitaya <miguelgotea.1@gmail.com>
2026-02-23 10:56:25 -0600

    2026-02-23 10:56:25

--- modulos/marketing/campanas_wsp.php
+++ modulos/marketing/campanas_wsp.php
@@ -25,7 +25,7 @@ require_once '../../core/layout/header_universal.php';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Campañas WhatsApp</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
-    <link rel="icon" href="../../assets/img/icon12.png" type="image/png">
+    <link rel="icon" href="../../core/assets/img/icon12.png" type="image/png">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.css">
     <link rel="stylesheet" href="/assets/css/global_tools.css?v=<?php echo mt_rand(1, 10000); ?>">
