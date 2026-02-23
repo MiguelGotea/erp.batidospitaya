64fe1b9048a38ad710b13bbef98f0bed7773b0c4
BatidosPitaya <miguelgotea.1@gmail.com>
2026-02-22 22:57:55 -0600

    2026-02-22 22:57:55

--- modulos/marketing/ajax/campanas_wsp_reset_sesion.php
+++ modulos/marketing/ajax/campanas_wsp_reset_sesion.php
@@ -9,7 +9,6 @@ require_once '../../../core/auth/auth.php';
 require_once '../../../core/permissions/permissions.php';
 require_once '../../../core/database/conexion.php';
 
-
 header('Content-Type: application/json; charset=utf-8');
 
 $usuario = obtenerUsuarioActual();
