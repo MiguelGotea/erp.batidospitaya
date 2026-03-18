17ac5b477cf0f5c1a5e1b5e00dfb5e1dfbdd3c34
BatidosPitaya <miguelgotea.1@gmail.com>
2026-03-17 18:59:26 -0600

    2026-03-17 18:59:26

--- modulos/compras/reembolsos_ia_historial.php
+++ modulos/compras/reembolsos_ia_historial.php
@@ -81,7 +81,7 @@ $historial = $stmtHist->fetchAll(PDO::FETCH_ASSOC);
                     <div class="card-body p-0">
                         <div class="table-responsive">
                             <table class="table table-hover mb-0">
-                                <thead class="table-light">
+                                <thead>
                                     <tr>
                                         <th>Fecha</th>
                                         <th>Proveedor</th>
